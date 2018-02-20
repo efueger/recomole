143a4e81afce2eb6dc4fa80b2532834873b274e4
Søren Hoilund Mollerup <shm@dbc.dk>
2018-02-20 14:06:12 +0100

    lang test devel

--- src/recomole/tests/test_loan_recommender.py
+++ src/recomole/tests/test_loan_recommender.py
@@ -129,6 +129,21 @@ class TestFilterCreator(unittest.TestCase):
         actual = [r['pid'] for r in recommendations]
         self.assertEqual(expected, actual)
 
+    def test_language_filter_1_language(self):
+        arguments = {'like': ['870970-basis:28634560'], 'filters': {'authorFlood': 2, 'language': ['eng']}}
+        recommendations, timings = self.recommender(**arguments)
+        print("REC1", [r['pid'] for r in recommendations])
+
+    def test_language_filter_2_language(self):
+        arguments = {'like': ['870970-basis:28634560'], 'filters': {'authorFlood': 2, 'language': ['dan', 'eng']}}
+        recommendations, timings = self.recommender(**arguments)
+        print("REC2", [r['pid'] for r in recommendations])
+
+    # def test_material_type_filter_1_material_type(self):
+    #     pass
+    # def test_material_type_filter_2_material_type(self):
+    #     pass
+
 # 870970-basis:52770831       |     7625 | 2016-12-13 14:14:03.411 | {"dk5": ["64.11"], "date": ["2016"], "type": ["Bog"], "title": ["Food from the fire"], "creator": ["Niklas Ekstedt"], "subject": ["bål", "Outdoor cooking", "kogebøger", "bålmad", "Cooking, Scandinavian", "madlavning", "Madlavning i alm. for særligt apparatur", "Food and Drink", "opskrifter"], "language": ["eng"], "contributor": ["Liz Haarala Hamilton", "Max Haarala Hamilton"], "subject_dbc": ["bål", "madlavning", "kogebøger", "opskrifter", "bålmad"], "subject_dbc_f": ["bål", "madlavning", "bålmad"], "subject_dbc_o": ["kogebøger", "opskrifter"]}                            | f
 #  870970-basis:52932319       |  6394635 | 2017-03-15 16:25:16.211 | {"dk5": ["78.7941"], "date": ["2016"], "type": ["Node"], "title": ["Just sing it!"], "subject": ["Danmark", "vokal", "2010-2019", "kor", "Antologier af rock og moderne folkemusik", "rock", "pop", "rytmisk kor"], "language": ["dan"], "contributor": ["Line Groth"], "subject_dbc": ["vokal", "kor", "rock", "pop", "rytmisk kor"], "subject_spat": ["Danmark"], "subject_temp": ["2010-2019"], "subject_dbc_m": ["vokal", "kor", "rock", "pop", "rytmisk kor"]}                                                                                                                               | f
 #  870970-basis:52932858
